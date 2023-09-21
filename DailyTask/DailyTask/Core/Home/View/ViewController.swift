a40491420c3a2a028d05fd61c2fdafeb573adfba
Eduardo R Vieira <eduardovieirayt@gmail.com>
2023-09-21 09:44:38 -0300

    change color

--- DailyTask/DailyTask/Core/Home/View/ViewController.swift
+++ DailyTask/DailyTask/Core/Home/View/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController, Coordinating {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 220, height: 55))
         view.addSubview(button)
         button.center = view.center
-        button.backgroundColor = UIColor(.bla)
+        button.backgroundColor = UIColor(.customRed)
         button.setTitleColor(.white, for: .normal)
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         button.setTitle("Tap Me!", for: .normal)
