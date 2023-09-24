bfa1c5b283a4bc79721efddbc2f25a309f4b020e
Eduardo R Vieira <eduardovieirayt@gmail.com>
2023-09-24 00:13:52 -0300

    Create of subTask and DatePicker

---
+++ DailyTask/DailyTask/Core/Components/DatePickerComponent.swift
@@ -0,0 +1,20 @@
+//
+//  DatePickerComponent.swift
+//  DailyTask
+//
+//  Created by Eduardo on 23/09/23.
+//
+
+import UIKit
+
+class DatePickerComponent: UIDatePicker {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
