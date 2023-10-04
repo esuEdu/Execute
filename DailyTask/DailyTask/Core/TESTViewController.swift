4da2fc1eda977601961a5a4f36cc22dd37010bf0
Leonardo Mesquita Alves <leonardo.pro.alves@gmail.com>
2023-10-04 16:42:07 -0300

    ScrollView da lista de projetos funcionando

--- DailyTask/DailyTask/Core/TESTViewController.swift
+++ DailyTask/DailyTask/Core/TESTViewController.swift
@@ -2,76 +2,44 @@ import UIKit
 
 class TESTViewController: UIViewController {
     
-    let scrollView: UIScrollView = {
-        let sv = UIScrollView()
-        sv.backgroundColor = .systemYellow
-        sv.translatesAutoresizingMaskIntoConstraints = false
-        return sv
-    }()
+    let textField = TextFieldComponent()
     
-    let contentView: UIStackView = {
-        let view = UIStackView()
-        view.backgroundColor = .systemCyan
-        view.axis = .vertical
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    let subview1: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.heightAnchor.constraint(equalToConstant: 300).isActive = true
-        view.backgroundColor = .systemRed
-        return view
-    }()
-    
-    let subview2: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.heightAnchor.constraint(equalToConstant: 500).isActive = true
-        view.backgroundColor = .systemMint
-        return view
-    }()
-    
-    let subview3: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        view.backgroundColor = .systemPurple
-        return view
-    }()
+    let scrollView = ScrollContainerComponent()
+    let container = ContainerProjectsList(title: "Bom dia eh o caralho seu bando de merda", description: "Um dia o patinho feio será morto", percentage: "0")
+    let container2 = ContainerProjectsList(title: "Bom noite", description: "Um dia botafogoo patinho feio será morto e o ganso inocente será o culpado", percentage: "66")
+    let container3 = ContainerProjectsList(title: "Bom flamengodia", description: "Um dia o patinho feio será morto", percentage: "100")
+    let container4 = ContainerProjectsList(title: "Bom noite", description: "Um dia o flamengopatinho feio será morto", percentage: "66")
+    let container5 = ContainerProjectsList(title: "Bom dia", description: "Um dia o patinho feio será morto", percentage: "100")
+    let container6 = ContainerProjectsList(title: "Bom noite", description: "Um dia vascoo patinho feio será morto", percentage: "66")
+    let container7 = ContainerProjectsList(title: "Bom dia", description: "Um dia o vas patinho feio será morto", percentage: "0")
+    let container8 = ContainerProjectsList(title: "Bom noite", description: "Umflamengo dia o patinho feio será morto", percentage: "66")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemGroupedBackground
-        
+        view.backgroundColor = .systemBackground
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(scrollView)
-        scrollView.addSubview(contentView)
+        scrollView.addNewElements(container)
+        scrollView.addNewElements(container2)
+        scrollView.addNewElements(container3)
+        scrollView.addNewElements(container4)
+        scrollView.addNewElements(container5)
+        scrollView.addNewElements(container6)
+        scrollView.addNewElements(container7)
+        scrollView.addNewElements(container8)
+        addAllConstraints()
         
-        
-        NSLayoutConstraint.activate([
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            
-            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-            
-        ])
-        
-        contentView.addArrangedSubview(subview1)
-        contentView.addArrangedSubview(subview2)
-        contentView.addArrangedSubview(subview3)
-                
+        scrollView.contains("")
     }
     
-    @objc func teste(){
-        print("Eita nois")
+    func addAllConstraints(){
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
     
 }
