b225465b1b3eb8021a3ed8f320aec00b2784781b
vhparaujo <vhugopacheco59@gmail.com>
2023-10-06 17:01:51 -0300

    Name of the project done

--- DailyTask/DailyTask/Core/Components/AllTaskListComponts.swift
+++ DailyTask/DailyTask/Core/Components/AllTaskListComponts.swift
@@ -14,11 +14,13 @@ class AllTaskListComponts: UIView {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.distribution = .equalSpacing
+      stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
     let icon: ChooseIconComponent = {
         let icon = ChooseIconComponent()
+      icon.translatesAutoresizingMaskIntoConstraints = false
         return icon
     }()
     
@@ -26,27 +28,32 @@ class AllTaskListComponts: UIView {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.distribution = .equalSpacing
+      stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
     init(titleTask: String, timeLabel: String) {
         super.init(frame: .zero)
+      
+      self.backgroundColor = .systemGray2
         
         let titleLabel = LabelComponent(text: titleTask, accessibilityLabel: titleTask)
         
         let timeLabel = LabelComponent(text: "Start at \(timeLabel)", accessibilityLabel: "Start at \(timeLabel)")
-        
-        texts.addArrangedSubview(titleLabel)
-        texts.addArrangedSubview(timeLabel)
+      
+      self.addSubview(mainStack)
+      
+      texts.addArrangedSubview(titleLabel)
+      texts.addArrangedSubview(timeLabel)
         
         mainStack.addArrangedSubview(icon)
         mainStack.addArrangedSubview(texts)
         
         NSLayoutConstraint.activate([
-            mainStack.topAnchor.constraint(equalTo: topAnchor),
-            mainStack.leadingAnchor.constraint(equalTo: leadingAnchor),
-            mainStack.trailingAnchor.constraint(equalTo: trailingAnchor),
-            mainStack.bottomAnchor.constraint(equalTo: bottomAnchor),
+          mainStack.topAnchor.constraint(equalTo: self.topAnchor),
+          mainStack.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+          mainStack.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+          mainStack.bottomAnchor.constraint(equalTo: self.bottomAnchor),
         ])
     }
     
