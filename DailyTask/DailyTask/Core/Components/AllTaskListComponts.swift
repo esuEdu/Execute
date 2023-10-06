baf6321c2302f0167ff5cf2a91af827848d68995
Luca <paesberaldiluca@gmail.com>
2023-10-06 17:02:17 -0300

    Fixed AllTaskListComponts, added the rounded checkbox to it as well.

--- DailyTask/DailyTask/Core/Components/AllTaskListComponts.swift
+++ DailyTask/DailyTask/Core/Components/AllTaskListComponts.swift
@@ -13,19 +13,45 @@ class AllTaskListComponts: UIView {
     let mainStack: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
-        stackView.distribution = .equalSpacing
+        stackView.distribution = .fill
+        stackView.backgroundColor = UIColor.red
+        stackView.layer.cornerRadius = 10
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 10, left: 12, bottom: 10, right: 12)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
     let icon: ChooseIconComponent = {
         let icon = ChooseIconComponent()
+        icon.translatesAutoresizingMaskIntoConstraints = false
         return icon
+    }()
+    
+    let roundedCheckbox: RoundedCheckbox = {
+        let roundedCheckbox = RoundedCheckbox()
+        roundedCheckbox.layer.zPosition = 10
+        roundedCheckbox.translatesAutoresizingMaskIntoConstraints = false
+        return roundedCheckbox
     }()
     
     let texts: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.distribution = .equalSpacing
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 0)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    let stackCheckbox: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalCentering
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
@@ -35,18 +61,26 @@ class AllTaskListComponts: UIView {
         let titleLabel = LabelComponent(text: titleTask, accessibilityLabel: titleTask)
         
         let timeLabel = LabelComponent(text: "Start at \(timeLabel)", accessibilityLabel: "Start at \(timeLabel)")
+    
+        self.addSubview(mainStack)
         
         texts.addArrangedSubview(titleLabel)
         texts.addArrangedSubview(timeLabel)
         
+        stackCheckbox.addArrangedSubview(roundedCheckbox)
+        
         mainStack.addArrangedSubview(icon)
         mainStack.addArrangedSubview(texts)
+        mainStack.addArrangedSubview(stackCheckbox)
         
         NSLayoutConstraint.activate([
             mainStack.topAnchor.constraint(equalTo: topAnchor),
             mainStack.leadingAnchor.constraint(equalTo: leadingAnchor),
             mainStack.trailingAnchor.constraint(equalTo: trailingAnchor),
             mainStack.bottomAnchor.constraint(equalTo: bottomAnchor),
+            icon.heightAnchor.constraint(equalToConstant: 50),
+            icon.widthAnchor.constraint(equalToConstant: 50),
+            roundedCheckbox.heightAnchor.constraint(greaterThanOrEqualTo: roundedCheckbox.heightAnchor, multiplier: 0)
         ])
     }
     
--- DailyTask/DailyTask/Core/Home/HomeView.swift
+++ DailyTask/DailyTask/Core/Home/HomeView.swift
@@ -16,18 +16,32 @@ class HomeView: UIViewController {
         view.backgroundColor = .systemBackground
         
         // Create a custom button
-        let teste: DateAndCalendarComponent = {
-            let teste = DateAndCalendarComponent(date: "fadfaf")
-            teste.translatesAutoresizingMaskIntoConstraints = false
-            return teste
+//        let teste: DateAndCalendarComponent = {
+//            let teste = DateAndCalendarComponent(date: "fadfaf")
+//            teste.translatesAutoresizingMaskIntoConstraints = false
+//            return teste
+//        }()
+        
+        let allTaskListComponts: AllTaskListComponts = {
+            let stackView = AllTaskListComponts(titleTask: "Joe Mama", timeLabel: "5:00")
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            stackView.clipsToBounds = true
+            return stackView
         }()
         
-        view.addSubview(teste)
+//        view.addSubview(teste)
+        view.addSubview(allTaskListComponts)
         
         NSLayoutConstraint.activate([
-            teste.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            teste.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            teste.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9)
+//            teste.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            teste.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+//            teste.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            
+            allTaskListComponts.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            allTaskListComponts.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            allTaskListComponts.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9)
+            
+            
         ])
     }
     
